57f78435658cf3bf2967194d36bef9e7713b21c2
looziolooz <lorenzo.dastoli@gmail.com>
2025-06-25 15:47:02 +0200

    first exercise

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let favoritMovies = ["Beautiful mind", "Avengers", "Matrix"];
+favoritMovies.push("Fargo");
+console.log(favoritMovies);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let favoritMovies: string [] = ["Beautiful mind", "Avengers", "Matrix"];
+
+favoritMovies.push("Fargo")
+
+console.log(favoritMovies);

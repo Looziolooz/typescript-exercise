075ddf8024cd6cc5730b146bae014f18b6a844ad
looziolooz <lorenzo.dastoli@gmail.com>
2025-06-25 16:10:37 +0200

    for...of loop + if..else

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,25 @@
 "use strict";
 let favoritMovies = ["Beautiful mind", "Avengers", "Matrix"];
 favoritMovies.push("Fargo");
+favoritMovies.push("Pulp Fiction");
 console.log(favoritMovies);
+favoritMovies.pop();
+console.log(favoritMovies);
+for (let favoritMovie of favoritMovies) {
+    console.log("Movies: " + favoritMovie);
+}
+;
+let score = 89;
+if (score >= 90) {
+    console.log("Excellent Score");
+}
+else if (score >= 70) {
+    console.log("Good Score");
+}
+else {
+    console.log("Needs improvement");
+}
+;
+console.log(score);
+let number = [10, 25, 5, 30, 15];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
 let favoritMovies: string [] = ["Beautiful mind", "Avengers", "Matrix"];
 
-favoritMovies.push("Fargo")
+favoritMovies.push("Fargo");
+favoritMovies.push("Pulp Fiction");
 
 console.log(favoritMovies);
+
+favoritMovies.pop();
+console.log(favoritMovies);
+
+
+for(let favoritMovie of favoritMovies) {
+  console.log("Movies: "+ favoritMovie)
+};
+
+
+let score: number = 89;
+
+if (score >= 90) {
+  console.log("Excellent Score")
+}else if (score >=70) {
+  console.log("Good Score")
+}else {
+  console.log("Needs improvement")
+};
+
+console.log(score);
+
+
+
+let number: number[] = [10, 25, 5, 30, 15]
